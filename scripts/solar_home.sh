a710023782faff5dcd437508acfd6d8a1a862f07
Cody Lewis <hello@codymlewis.com>
2023-08-31 10:49:39 +1000

    Added full solar home performance evaluation

--- scripts/solar_home.sh
+++ scripts/solar_home.sh
@@ -3,5 +3,5 @@
 LENGTH=$(python scripts/count_experiments.py performance)
 
 for i in $(seq $LENGTH); do
-    python performance_eval.py -i $i
+    python solar_home_eval.py -i $i
 done
--- solar_home_eval.py
+++ solar_home_eval.py
@@ -1,13 +1,29 @@
+from typing import List, Dict
 from functools import partial
+import argparse
 import pickle
+import json
+import time
+import logging
+import os
 import numpy as np
+from numpy.typing import NDArray
 import einops
 import sklearn.metrics as skm
 import scipy.optimize as sp_opt
-from tqdm import trange
-import json
+from tqdm import tqdm, trange
 
 import src.data_manager
+
+
+logger = logging.getLogger("dylon")
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(logging.Formatter('| %(name)s %(levelname)s @ %(asctime)s in %(filename)s:%(lineno)d | %(message)s'))
+ch.setStream(tqdm)
+ch.terminator = ""
+logger.addHandler(ch)
 
 
 class RidgeModel:
@@ -49,12 +65,17 @@ class Server:
         self.model = RidgeModel()
         self.clients = clients
         self.config = config
+        logger.info("Server initialized with %d clients", len(clients))
 
     def fit(self):
+        start_time = time.time()
+        logger.info("Server starting training for %d rounds", self.config['num_rounds'])
         for r in (pbar := trange(self.config['num_rounds'])):
-            loss = self.step()
-            pbar.set_postfix_str(f"loss: {loss:.3f}")
-        return loss
+            metrics = self.step()
+            pbar.set_postfix_str(f"loss: {metrics['loss']:.3f}")
+            logger.info("Loss at the end of round %d: %f", r + 1, metrics['loss'])
+        logger.info("Server completed training in %f seconds", time.time() - start_time)
+        return metrics
 
     def step(self):
         all_params, all_losses, all_samples = [], [], []
@@ -65,9 +86,14 @@ class Server:
             all_samples.append(num_samples)
         all_grads = [cp - self.model.parameters for cp in all_params]
         self.model.parameters += np.average(all_grads, weights=all_samples, axis=0)
-        return np.average(all_losses, weights=all_samples)
+        return {
+            "loss": np.average(all_losses, weights=all_samples),
+            "cosine similarity": cosine_similarity(all_grads),
+        }
 
     def analytics(self):
+        start_time = time.time()
+        logger.info("Server starting analytics")
         all_preds, all_Y_test = [], []
         for c in self.clients:
             client_preds, client_Y_test = c.analytics(self.model.parameters.copy(), self.config)
@@ -75,6 +101,7 @@ class Server:
             all_Y_test.append(client_Y_test)
         preds = np.concatenate(all_preds)
         Y_test = np.concatenate(all_Y_test)
+        logger.info("Server completed analytics in %f seconds", time.time() - start_time)
         return {
             "MAE": skm.mean_absolute_error(Y_test, preds),
             "RMSE": np.sqrt(skm.mean_squared_error(Y_test, preds)),
@@ -90,33 +117,85 @@ class Server:
         }    
 
 
-class MiddleServer:
-    def __init__(self, clients):
-        self.clients = clients
+def cosine_similarity(client_parameters):
+    client_parameters = [cp.reshape(-1) for cp in client_parameters]
+    similarity_matrix = skm.pairwise.cosine_similarity(client_parameters) - np.eye(len(client_parameters))
+    return similarity_matrix.sum() / (len(client_parameters) * (len(client_parameters) - 1))
 
-    def step(self, parameters, config):
-        for e in range(config['num_episodes']):
+class MiddleServer:
+    def __init__(self, clients, config):
+        self.aggregator = KickbackMomentum() if config.get("mu1") else FedAVG()
+        self.clients = clients
+        logger.info("Middle server initialized with %d clients", len(clients))
+
+    def step(self, parameters, config, finetuning=False):
+        start_time = time.time()
+        num_episodes = config['num_finetune_episodes'] if finetuning else config['num_episodes']
+        logger.info("Middle server starting training for %d episodes", num_episodes)
+        for e in range(num_episodes):
             all_params, all_losses, all_samples = [], [], []
             for c in self.clients:
                 params, loss, num_samples = c.step(parameters.copy(), config)
                 all_params.append(params)
                 all_losses.append(loss)
                 all_samples.append(num_samples)
-            all_grads = [cp - parameters for cp in all_params]
-            parameters += np.average(all_grads, weights=all_samples, axis=0)
-        return parameters, np.average(all_losses, weights=all_samples), sum(all_samples)
+            parameters = self.aggregator.aggregate(all_params, all_samples, parameters, config)
+        loss = np.average(all_losses, weights=all_samples)
+        logger.info("Middle server completed training in %f seconds", time.time() - start_time)
+        logger.info("Middle server loss: %f", loss)
+        return parameters, loss, sum(all_samples)
 
     def analytics(self, parameters, config):
+        start_time = time.time()
+        logger.info("Middle server starting analytics")
         if config.get("num_finetune_episodes"):
-            for e in range(config["num_finetune_episodes"]):
-                parameters, _, _ = self.step(parameters, config)
+            logger.info("Middle server is finetuning")
+            parameters, _, _ = self.step(parameters, config, finetuning=True)
 
         all_preds, all_Y_test = [], []
         for c in self.clients:
             preds, Y_test = c.analytics(parameters.copy(), config)
             all_preds.append(preds)
             all_Y_test.append(Y_test)
+        logger.info("Middle server completed analytics in %f seconds", time.time() - start_time)
         return np.concatenate(all_preds), np.concatenate(all_Y_test)
+
+
+class FedAVG:
+    def aggregate(
+        self,
+        client_parameters: List[NDArray],
+        client_samples: List[int],
+        parameters: NDArray,
+        config: Dict[str, str | int | float]
+    ) -> NDArray:
+        return np.average(client_parameters, weights=client_samples, axis=0)
+
+
+
+class KickbackMomentum:
+    def __init__(self):
+        self.momentum = None
+        self.prev_parameters = None
+        self.episode = 0
+
+    def aggregate(
+        self,
+        client_parameters: List[NDArray],
+        client_samples: List[int],
+        parameters: NDArray,
+        config: Dict[str, str | int | float]
+    ) -> NDArray:
+        if self.episode % config['num_episodes'] == 0:
+            if self.momentum is None:
+                self.momentum = np.zeros_like(parameters)
+                self.prev_parameters = parameters.copy()
+            else:
+                self.momentum = config["mu1"] * self.momentum + (parameters - self.prev_parameters)
+                self.prev_parameters = parameters.copy()
+        self.episode += 1
+        grads = np.average([cp - parameters for cp in client_parameters], weights=client_samples, axis=0)
+        return self.prev_parameters + config["mu2"] * self.momentum + grads
 
 
 class Client:
@@ -168,18 +247,39 @@ def load_customer_regions():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Perform experiments evaluating the fairness when clients drop out from colloboration.")
+    parser.add_argument("-i", "--id", type=int, default=1, help="Which of the experiments in the config to perform (counts from 1).")
+    args = parser.parse_args()
+    # TODO: Add attack and fairness evals
+
+    start_time = time.time()
+    with open("configs/performance.json", 'r') as f:
+        experiment_config = src.common.get_experiment_config(json.load(f), args.id)
+    print(f"Performing experiment with {experiment_config=}")
+
     client_data, X_test, Y_test = load_data()
     regions = load_customer_regions()
-    # TODO: Momentum and logging
 
     server = Server(
-        [MiddleServer([Client(client_data[r]) for r in region]) for region in regions],
-        {"num_rounds": 5, "num_episodes": 1, "num_epochs": 1}
+        [MiddleServer([Client(client_data[r]) for r in region], experiment_config) for region in regions],
+        experiment_config
     )
-    loss = server.fit()
+    training_metrics = server.fit()
 
-    print("Client models:")
-    print(server.analytics())
+    testing_metrics = server.analytics()
+    centralised_metrics = server.evaluate(X_test, Y_test)
 
-    print("Server model:")
-    print(server.evaluate(X_test, Y_test))
+    print(f"{training_metrics=}")
+    print(f"{testing_metrics=}")
+    print(f"{centralised_metrics=}")
+
+    results = {"train": training_metrics, "test": testing_metrics, "centralised": centralised_metrics}
+    os.makedirs("results", exist_ok=True)
+    filename = "results/solar_home_performance_{}.json".format(
+        '_'.join([f'{k}={v}' for k, v in experiment_config.items() if k not in ['repeat']]),
+    )
+    with open(filename, "w") as f:
+        json.dump(results, f)
+    print(f"Saved results to {filename}")
+
+    print(f"Experiment took {time.time() - start_time} seconds")
