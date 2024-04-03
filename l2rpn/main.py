3bcfedbf460ed98f0177d568310ad30b86162e3e
Cody Lewis <hello@codymlewis.com>
2024-04-03 13:39:33 +1100

    Cleaned up l2rpn main

--- l2rpn/main.py
+++ l2rpn/main.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Tuple, Dict
 import argparse
 from functools import partial
 import os
@@ -46,7 +47,7 @@ def setup(
     global_params = forecast_model.init(jax.random.PRNGKey(seed), jnp.zeros((1, 2 * forecast_window + 2)))
     substation_ids = substation_data['ids']
 
-    if attack == "":
+    if attack == "none":
         adversary_type = fl.Client
     elif attack == "empty":
         adversary_type = adversaries.EmptyUpdater
@@ -89,36 +90,90 @@ def setup(
     return server
 
 
-def test_model(test_env, agent, server, forecast_window, fairness):
+def train(
+    server: fl.Server,
+    episodes: int,
+    timesteps: int,
+    batch_size: int,
+    forecast_window: int,
+    drop_episode: int,
+) -> float:
+    training_data = load_file('data/training.safetensors')
+    for e in trange(episodes):
+        server.reset()
+        for t in range(timesteps):
+            obs_load_p = training_data[f"E{e}T{t}:load_p"]
+            obs_gen_p = training_data[f"E{e}T{t}:gen_p"]
+            obs_time = training_data[f"E{e}T{t}:time"]
+            server.add_data(obs_load_p, obs_gen_p, obs_time)
+        if (e + 1) * timesteps > (batch_size + forecast_window):
+            cs = server.step()
+        if e == drop_episode - 1:
+            server.drop_clients()
+    return cs
+
+
+def test(
+    server: fl.Server,
+    episodes: int,
+    timesteps: int,
+    forecast_window: int,
+    fairness: bool,
+    args_dict: Dict[str, str | int | float | bool]
+) -> Tuple[str, str]:
+    testing_data = load_file('data/testing.safetensors')
     server.setup_test()
-    obs = test_env.reset()
-    reward = train_env.reward_range[0]
-    done = False
     client_forecasts, true_forecasts = [], []
     if fairness:
         dropped_cfs, dropped_tfs = [], []
-    for i in range(100):
-        test_result = server.add_test_data(obs, fairness=fairness)
-        if fairness:
-            true_forecast, client_forecast, dropped_tf, dropped_cf = test_result
-        else:
-            true_forecast, client_forecast = test_result
-        true_forecasts.append(true_forecast)
-        client_forecasts.append(client_forecast)
-        if fairness:
-            dropped_tfs.append(dropped_tf)
-            dropped_cfs.append(dropped_cf)
-        act = agent.act(obs, reward, done)
-        obs, reward, done, info = test_env.step(act)
-        if done:
-            obs = test_env.reset()
+    for e in trange(episodes):
+        server.reset()
+        for t in range(timesteps):
+            obs_load_p = testing_data[f"E{e}T{t}:load_p"]
+            obs_gen_p = testing_data[f"E{e}T{t}:gen_p"]
+            obs_time = testing_data[f"E{e}T{t}:time"]
+            test_result = server.add_test_data(obs_load_p, obs_gen_p, obs_time, fairness=fairness)
+            if fairness:
+                true_forecast, client_forecast, dropped_tf, dropped_cf = test_result
+            else:
+                true_forecast, client_forecast = test_result
+            true_forecasts.append(true_forecast)
+            client_forecasts.append(client_forecast)
+            if fairness:
+                dropped_tfs.append(dropped_tf)
+                dropped_cfs.append(dropped_cf)
     client_forecasts = np.array(client_forecasts[forecast_window - 1:-1])
     true_forecasts = np.array(true_forecasts[forecast_window - 1:-1])
     if fairness:
         dropped_cfs = np.array(dropped_cfs[forecast_window - 1:-1])
         dropped_tfs = np.array(dropped_tfs[forecast_window - 1:-1])
-        return client_forecasts, true_forecasts, dropped_cfs, dropped_tfs
-    return client_forecasts, true_forecasts
+    client_forecasts = client_forecasts.reshape(-1, 2)[forecast_window - 1:-1]
+    true_forecasts = true_forecasts.reshape(-1, 2)[forecast_window - 1:-1]
+    if fairness:
+        dropped_cfs = dropped_cfs.reshape(-1, 2)[forecast_window - 1:-1]
+        dropped_tfs = dropped_tfs.reshape(-1, 2)[forecast_window - 1:-1]
+
+    header = "mae,rmse,r2_score,"
+    if fairness:
+        header += "dropped mae,dropped rmse,dropped r2_score,"
+    header += ",".join(args_dict.keys())
+    results = "{},{},{},".format(
+        metrics.mean_absolute_error(true_forecasts, client_forecasts),
+        math.sqrt(metrics.mean_squared_error(true_forecasts, client_forecasts)),
+        metrics.r2_score(true_forecasts, client_forecasts),
+    )
+    if fairness:
+        results += "{},{},{},".format(
+            metrics.mean_absolute_error(dropped_tfs, dropped_cfs),
+            math.sqrt(metrics.mean_squared_error(dropped_tfs, dropped_cfs)),
+            metrics.r2_score(dropped_tfs, dropped_cfs),
+        )
+    results += ",".join([str(v) for v in args_dict.values()])
+    if cs:
+        header += ",cosine_similarity"
+        results += f",{cs}"
+    logger.info(f"{results=}")
+    return header, results
 
 
 if __name__ == "__main__":
@@ -145,8 +200,10 @@ if __name__ == "__main__":
     parser.add_argument("--middle-server-aggregator", type=str, default="fedavg",
                         help="Aggregation algorithm to use at the FL middle server.")
     parser.add_argument("--fairness", action="store_true", help="Perform the fairness evaluation.")
-    parser.add_argument("--attack", type=str, default="",
+    parser.add_argument("--attack", type=str, default="none",
                         help="Perform model poisoning on the federated learning model.")
+    parser.add_argument('--drop-point', type=float, default=1.1,
+                        help="Percent of episodes to pass before dropping clients")
     args = parser.parse_args()
 
     print(f"Running experiment with {vars(args)}")
@@ -154,9 +211,7 @@ if __name__ == "__main__":
     start_time = time.time()
     rng = np.random.default_rng(args.seed)
     rngkey = jax.random.PRNGKey(args.seed)
-
-    if args.fairness:
-        drop_episode = round(args.episodes * 0.4)
+    drop_episode = round(args.episodes * args.drop_point)
 
     server = setup(
         load_file('data/substation.safetensors'),
@@ -177,66 +232,21 @@ if __name__ == "__main__":
         seed=args.seed,
     )
     num_clients = server.num_clients
-    training_data = load_file('data/training.safetensors')
 
-    logger.info("Generating data with simulations of the grid and training the models")
-    for e in (pbar := trange(args.episodes)):
-        server.reset()
-        for t in range(args.timesteps):
-            obs_load_p = training_data[f"E{e}T{t}:load_p"]
-            obs_gen_p = training_data[f"E{e}T{t}:gen_p"]
-            obs_time = training_data[f"E{e}T{t}:time"]
-            server.add_data(obs_load_p, obs_gen_p, obs_time)
-        if (e + 1) * args.timesteps > (args.batch_size + args.forecast_window):
-            cs = server.step()
-        if args.fairness and e == drop_episode - 1:
-            server.drop_clients()
+    cs = train(server, args.episodes, args.timesteps, args.batch_size, args.forecast_window, drop_episode)
 
-    # # The testing phase
-    # logger.info("Testing the trained model.")
-    # test_env = grid2op.make(env_name + "_test", backend=LightSimBackend(), reward_class=LinesCapacityReward)
-    # test_results = test_model(test_env, agent, server, args.forecast_window, args.fairness)
-    # if args.fairness:
-    #     client_forecasts, true_forecasts, dropped_cfs, dropped_tfs = test_results
-    # else:
-    #     client_forecasts, true_forecasts = test_results
-    # client_forecasts = client_forecasts.reshape(-1, 2)[args.forecast_window - 1:-1]
-    # true_forecasts = true_forecasts.reshape(-1, 2)[args.forecast_window - 1:-1]
-    # if args.fairness:
-    #     dropped_cfs = dropped_cfs.reshape(-1, 2)[args.forecast_window - 1:-1]
-    #     dropped_tfs = dropped_tfs.reshape(-1, 2)[args.forecast_window - 1:-1]
+    logger.info("Testing the trained model.")
+    header, results = test(server, args.episodes, args.timesteps, args.forecast_window, args.fairness, vars(args))
 
-    # args_dict = vars(args)
-    # header = "mae,rmse,r2_score,"
-    # if args.fairness:
-    #     header += "dropped mae,dropped rmse,dropped r2_score,"
-    # header += ",".join(args_dict.keys())
-    # results = "{},{},{},".format(
-    #     metrics.mean_absolute_error(true_forecasts, client_forecasts),
-    #     math.sqrt(metrics.mean_squared_error(true_forecasts, client_forecasts)),
-    #     metrics.r2_score(true_forecasts, client_forecasts),
-    # )
-    # if args.fairness:
-    #     results += "{},{},{},".format(
-    #         metrics.mean_absolute_error(dropped_tfs, dropped_cfs),
-    #         math.sqrt(metrics.mean_squared_error(dropped_tfs, dropped_cfs)),
-    #         metrics.r2_score(dropped_tfs, dropped_cfs),
-    #     )
-    # results += ",".join([str(v) for v in args_dict.values()])
-    # if cs:
-    #     header += ",cosine_similarity"
-    #     results += f",{cs}"
-    # logger.info(f"{results=}")
-
-    # # Record the results
-    # os.makedirs("results", exist_ok=True)
-    # file_suffix = "attack" if args.attack else "fairness" if args.fairness else "performance"
-    # filename = f"results/l2rpn_{file_suffix}.csv"
-    # if not os.path.exists(filename):
-    #     with open(filename, 'w') as f:
-    #         f.write(header + "\n")
-    # with open(filename, 'a') as f:
-    #     f.write(results + "\n")
-    # print(f"Results written to {filename}")
+    # Record the results
+    os.makedirs("results", exist_ok=True)
+    file_suffix = "attack" if args.attack else "fairness" if args.fairness else "performance"
+    filename = f"results/l2rpn_{file_suffix}.csv"
+    if not os.path.exists(filename):
+        with open(filename, 'w') as f:
+            f.write(header + "\n")
+    with open(filename, 'a') as f:
+        f.write(results + "\n")
+    print(f"Results written to {filename}")
 
     print(f"Experiment took {time.time() - start_time} seconds")
