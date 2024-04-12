9d0091b21a1fe391146f37aba66dca02f7efe7ed
Cody Lewis <hello@codymlewis.com>
2024-04-12 14:40:56 +1000

    Trialled a new variation of topomean

--- topomean/plot.py
+++ topomean/plot.py
@@ -2,7 +2,14 @@ import argparse
 import time
 import numpy as np
 import scipy as sp
-import sklearn.cluster as skc
+import sklearn.metrics as skm
+
+
+def angle_cluster(samples: np.typing.NDArray, mean: np.typing.NDArray) -> np.typing.NDArray:
+    sims = skm.pairwise.cosine_similarity(samples - mean)
+    leaders = np.unravel_index(sims.argmin(), sims.shape)
+    groups = np.array([samples[sims[leader] >= 0].mean(0) for leader in leaders])
+    return groups
 
 
 if __name__ == "__main__":
@@ -15,7 +22,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(f"Experiment args: {vars(args)}")
     npoints = 10000
-    dimensions = 10
+    dimensions = 2
+    e1 = 0.1
+    e2 = 0.5
 
     start_time = time.time()
     rng = np.random.default_rng(args.seed)
@@ -26,53 +35,33 @@ if __name__ == "__main__":
     x = np.concatenate((honest_x, attack_x))
     mu = np.mean(x, axis=0)
     sigma = np.std(x)
+
     dists = sp.spatial.distance.cdist(x, x)
     mu_densities = np.linalg.norm(x - mu, axis=1) < sigma
     mu_dists = np.linalg.norm(x - mu, axis=1)
     sonar = np.array([
-        np.sum((mu_dists <= i * 0.1 * sigma) & (mu_dists > (i - 1) * 0.1 * sigma))
+        np.sum((mu_dists <= i * e1 * sigma) & (mu_dists > (i - 1) * e1 * sigma))
         for i in range(1, round(3 / 0.1))
     ])
     peak_indices = np.argwhere((sonar[1:-1] >= sonar[2:]) & (sonar[1:-1] > sonar[:-2])).reshape(-1) + 1
     centres = []
     for pi in peak_indices:
-        model = skc.KMeans(2)
-        model.fit(x[(mu_dists <= pi * 0.1 * sigma) & (mu_dists > (pi - 1) * 0.1 * sigma)])
-        centres.append(model.cluster_centers_)
+        considered_samples = x[(mu_dists <= pi * e1 * sigma) & (mu_dists > (pi - 1) * e1 * sigma)]
+        centres.append(angle_cluster(considered_samples, mu))
     centres = np.concatenate(centres)
-    smaller = centres[np.mean(centres - mu, axis=1) < np.mean(centres - mu)].mean(0).reshape(-1)
-    bigger = centres[np.mean(centres - mu, axis=1) >= np.mean(centres - mu)].mean(0).reshape(-1)
-    mu_density = mu_densities.sum()
-    smaller_density = np.sum(np.linalg.norm(x - smaller, axis=1) < sigma)
-    bigger_density = np.sum(np.linalg.norm(x - bigger, axis=1) < sigma)
-    print(f"{mu=}, {mu_density=}")
-    print(f"{smaller=}, {smaller_density=}, {bigger=}, {bigger_density=}")
-    max_density = max(mu_density, smaller_density, bigger_density)
-    agg = []
-    e1 = 0.05
-    print(f"{abs(smaller_density - bigger_density)=}, {e1 * max_density=}")
-    # if mu_density != max_density:
-    #     agg.append(smaller if smaller_density > bigger_density else bigger)
-    # elif abs(smaller_density - bigger_density) < e1 * max_density:
-    #     density_sum = smaller_density + mu_density + max_density
-    #     agg.extend([
-    #         smaller * (smaller_density / density_sum) * 3,
-    #         mu * (mu_density / density_sum) * 3,
-    #         bigger * (bigger_density / density_sum) * 3
-    #     ])
-    # else:
-    #     if smaller_density >= (1 - e1) * max_density:
-    #         agg.append(smaller)
-    #     elif bigger_density >= (1 - e1) * max_density:
-    #         agg.append(bigger)
-    #     else:
-    #         agg.append(mu)
-    #         agg.append(smaller if smaller_density > bigger_density else bigger)
+    print(f"{centres=}")
+    all_centres = angle_cluster(centres, mu)
+    all_centres = np.concatenate([all_centres, np.array([mu])])
+    print(f"{all_centres=}")
+    densities = np.array([np.sum(np.linalg.norm(x - c, axis=1) < sigma) for c in all_centres])
+    max_density = np.max(densities)
+    # densities[-1] *= e2
+    weights = sp.stats.norm.pdf(np.abs(densities - max_density), scale=e1*max_density)
+    p = weights / weights.sum()
+    print(f"{densities=}, {weights=}, {p=}")
+    agg_x = np.sum((all_centres.T * p).T, axis=0)
+    print(f"{agg_x=}")
+    print(f"{honest_x.mean(0)=}")
+    print(f"Error: {np.linalg.norm(agg_x - honest_x.mean(0))}")
 
-    density_sum = smaller_density + mu_density + max_density
-    agg.extend([
-        smaller * (smaller_density / density_sum),
-        mu * (mu_density / density_sum),
-        bigger * (bigger_density / density_sum),
-    ])
-    print(f"{np.sum(agg, axis=0)=}")
+    print(f"Experiment took {time.time() - start_time} seconds")
