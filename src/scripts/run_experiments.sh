acd5c0eb749276ddfe770650ede76550025f2774
Cody Lewis <hello@codymlewis.com>
2024-04-23 15:50:28 +1000

    Reduced number of experiments

--- src/scripts/run_experiments.sh
+++ src/scripts/run_experiments.sh
@@ -9,14 +9,14 @@ run_many_seeds () {
 
 for dataset in "l2rpn" "apartment" "solar_home"; do
     for attack in "none" "empty" "lie" "ipm"; do
-        for server_aggregator in "fedavg" "median" "topk" "krum" "trimmed_mean" "phocas" "geomedian" "kickback_momentum" "fedprox" "mrcs" "ssfgm" "space_sample_mean"; do
-            for ms_aggregator in "fedavg" "median" "topk" "krum" "trimmed_mean" "phocas" "geomedian" "kickback_momentum" "fedprox" "mrcs" "ssfgm" "space_sample_mean"; do
-                for drop_point in 0.4 0.8 1.1; do
+        for server_aggregator in "fedavg" "median" "krum" "trimmed_mean" "phocas" "geomedian" "fedprox" "ssfgm" "space_sample_mean"; do
+            for ms_aggregator in "fedavg" "topk" "geomedian" "kickback_momentum" "fedprox" "mrcs" "ssfgm" "space_sample_mean"; do
+                for drop_point in 0.4 1.1; do
                     if [ $attack == 'none' ]; then
-                        run_many_seeds "python main.py --dataset $dataset --attack $attack --server-aggregator $server_aggregator --middle-server-aggregator $ms_aggregator --drop-point $drop_point" 10
+                        run_many_seeds "python main.py --dataset $dataset --attack $attack --server-aggregator $server_aggregator --middle-server-aggregator $ms_aggregator --drop-point $drop_point" 5
                     else
-                        run_many_seeds "python main.py --dataset $dataset --attack $attack --server-aggregator $server_aggregator --middle-server-aggregator $ms_aggregator --drop-point $drop_point --pct-saturation 0.5 --pct-adversaries 1.0" 10
-                        run_many_seeds "python main.py --dataset $dataset --attack $attack --server-aggregator $server_aggregator --middle-server-aggregator $ms_aggregator --drop-point $drop_point --pct-saturation 1.0 --pct-adversaries 0.5" 10
+                        run_many_seeds "python main.py --dataset $dataset --attack $attack --server-aggregator $server_aggregator --middle-server-aggregator $ms_aggregator --drop-point $drop_point --pct-saturation 0.5 --pct-adversaries 1.0" 5
+                        run_many_seeds "python main.py --dataset $dataset --attack $attack --server-aggregator $server_aggregator --middle-server-aggregator $ms_aggregator --drop-point $drop_point --pct-saturation 1.0 --pct-adversaries 0.5" 5
                     fi
                 done
             done
